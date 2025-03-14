5cdb3391e5ace9a9488028a240d49707a529bf25
prabahar <prabahar.ravichandran@agr.gc.ca>
2025-03-14 12:00:46 +0000

    Move ufps from apptainer to Assets so it could be moved to AWS

--- MoveUFPS4AWS.sh
+++ MoveUFPS4AWS.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cd /gpfs/fs7/aafc/phenocart/PhenomicsProjects/DeploymentoverContainers/Assets/ufps_sandbox/home/
+rm -rf /gpfs/fs7/aafc/phenocart/PhenomicsProjects/DeploymentoverContainers/Assets/ufps
+
+mv /gpfs/fs7/aafc/phenocart/PhenomicsProjects/DeploymentoverContainers/Assets/ufps_sandbox/home/ufps /gpfs/fs7/aafc/phenocart/PhenomicsProjects/DeploymentoverContainers/Assets/
+
+
 
 
